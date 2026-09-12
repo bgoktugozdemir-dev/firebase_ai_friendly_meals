ceef64c70611f66b1efbf0ea977f6dbfa21f18d7
Göktuğ Özdemir <bgoktugozdemir@gmail.com>
2026-09-13 00:14:28 +0300

    feat: update App Check providers for debug mode

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:firebase_ai_friendly_meals/injection.dart';
 import 'package:firebase_ai_friendly_meals/ui/home/home_page.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
@@ -12,10 +13,15 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
   /// You can check [here](https://firebase.google.com/docs/app-check/flutter/debug-provider) for more info about App Check debug provider.
   await FirebaseAppCheck.instance.activate(
-    providerAndroid: const AndroidDebugProvider(),
-    providerApple: const AppleDebugProvider(),
+    providerAndroid: kDebugMode
+        ? const AndroidDebugProvider()
+        : const AndroidPlayIntegrityProvider(),
+    providerApple: kDebugMode
+        ? const AppleDebugProvider()
+        : const AppleAppAttestProvider(),
   );
   configureDependencies();
 
